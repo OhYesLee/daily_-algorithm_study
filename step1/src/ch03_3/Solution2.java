f077da574fb9e0508053d467226fa6996a386c68
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-11 15:13:26 +0900

    20221011

--- step1/src/ch03_3/Solution2.java
+++ step1/src/ch03_3/Solution2.java
@@ -3,6 +3,9 @@ package ch03_3;
 import java.util.Arrays;
 import java.util.Collections;
 
+/**
+ * String array를 사용하면 Collections.reverseOrder()를 사용해서 정렬 할 수 있다.
+ */
 public class Solution2 {
     public long solution2(long n) {
         String[] list = String.valueOf(n).split("");
