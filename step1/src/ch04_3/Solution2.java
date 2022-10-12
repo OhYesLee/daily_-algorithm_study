d4bd745d0ec77daa5328df67c90e56e19f26df7b
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-12 10:59:24 +0900

    20221012

--- step1/src/ch04_3/Solution2.java
+++ step1/src/ch04_3/Solution2.java
@@ -4,8 +4,11 @@ public class Solution2 {
     public int solution2(int n) {
         int answer = 0;
 
+        // 최소값부터 n -1 까지 반복문 반복
         for (int i = 2; i < n; i++) {
+            // 나머지가 1인 경우
             if (n % i == 1) {
+                // answer에 값 넣어주고 반복문 종료
                 answer = i;
                 break;
             }
