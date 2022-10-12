21b737c2c7b89652b9629cf18854acf1a0cb6e88
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-12 10:22:42 +0900

    20221012

---
+++ step1/src/ch04/Solution.java
@@ -0,0 +1,13 @@
+package ch04;
+
+/**
+ * 음수를 보고 어떻게 변환할지 고민하다가 프로젝트 때 사용한 Integer.parseInt() 메소드를 사용했다!
+ * parseInt는 문자열을 int형으로 변환하는 메서드다.
+ */
+public class Solution {
+    public int solution(String s) {
+        int n = 0;
+        return n = Integer.parseInt(s);
+    }
+
+}
