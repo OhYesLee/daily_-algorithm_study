a2619fd3f5be8e87e20e8817ea8a811fa3377621
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-20 11:59:28 +0900

    20221020

---
+++ step1/src/ch10/SolutionTestTwo.java
@@ -0,0 +1,10 @@
+package ch10;
+
+public class SolutionTestTwo {
+    public static void main(String[] args) {
+        SolutionOne solutionOne = new SolutionOne();
+        int[] win = {1, 2, 3, 4, 5, 6};
+        int[] lot = {1, 2, 3, 4, 5, 6};
+
+    }
+}
