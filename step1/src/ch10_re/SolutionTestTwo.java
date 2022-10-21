3870bb84f89d442c7982286d0359a129a1ff3086
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-21 14:39:33 +0900

    20221021

--- step1/src/ch10_re/SolutionTestTwo.java
+++ step1/src/ch10_re/SolutionTestTwo.java
@@ -1,10 +1,13 @@
 package ch10_re;
 
+import java.util.Scanner;
+
 public class SolutionTestTwo {
     public static void main(String[] args) {
-        SolutionOne solutionOne = new SolutionOne();
-        int[] win = {1, 2, 3, 4, 5, 6};
-        int[] lot = {1, 2, 3, 4, 5, 6};
-        System.out.println(solutionOne.solution(win, lot));
+        SolutionTwo solutionTwo = new SolutionTwo();
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+//        int[] win = {1, 2, 3, 4, 5, 6};
+//        int[] lot = {1, 2, 3, 4, 5, 6};
     }
 }
--- step1/src/ch10_re/SolutionTwo.java
+++ step1/src/ch10_re/SolutionTwo.java
@@ -1,5 +1,31 @@
 package ch10_re;
 
-public class SolutionTwo {
+import java.util.Arrays;
 
+public class SolutionTwo {
+//    int[] win = {1, 2, 3, 4, 5, 6};
+//    int[] lot = {1, 2, 3, 4, 5, 6};
+    public int[] solutiontwo(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int matchCnt = 0;
+        int zeroCnt = 0;
+
+        Arrays.sort(lottos);
+        Arrays.sort(win_nums);
+
+        // 최저 순위
+        for (int lotto : lottos) {
+            if (Arrays.binarySearch(win_nums, lotto) >= 0)
+                matchCnt++;
+
+            if (lotto == 0)
+                zeroCnt++;
+        }
+        answer[1] = Math.min(7 - matchCnt, 6);
+
+        // 최고 순위
+        answer[0] = Math.min(7 - (matchCnt + zeroCnt), 6);
+
+        return answer;
+    }
 }
