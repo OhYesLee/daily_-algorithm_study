c3e14b362d611d68e12db00caa4470fc392a5348
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-22 22:31:37 +0900

    20221022

--- step1/src/ch10_re/SolutionTestTwo.java
+++ step1/src/ch10_re/SolutionTestTwo.java
@@ -1,13 +1,14 @@
 package ch10_re;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SolutionTestTwo {
     public static void main(String[] args) {
+        int[] win = {1, 2, 3, 4, 5, 6};
+        int[] lot = {1, 2, 3, 4, 5, 6};
         SolutionTwo solutionTwo = new SolutionTwo();
         Scanner sc = new Scanner(System.in);
-        System.out.println();
-//        int[] win = {1, 2, 3, 4, 5, 6};
-//        int[] lot = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(solutionTwo.solutiontwo(lot, win)));
     }
 }
