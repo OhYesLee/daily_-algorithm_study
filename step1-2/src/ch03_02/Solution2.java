b6c7bd3bb7f598f7b68b760841bacbb3eff7a0f8
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-23 17:54:33 +0900

    20221023

---
+++ step1-2/src/ch03_02/Solution2.java
@@ -0,0 +1,13 @@
+package ch03_02;
+
+public class Solution2 {
+    public int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            answer = answer + (n % 10);
+            n = n / 10;
+        }
+        return answer;
+    }
+}
