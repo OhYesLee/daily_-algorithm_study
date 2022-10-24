b22d1332134177c296856a55042d730d39b6e20e
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-24 21:41:47 +0900

    20221024

--- step1-2/src/ch04_02/solutionOne.java
+++ step1-2/src/ch04_02/solutionOne.java
@@ -3,6 +3,14 @@ package ch04_02;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * 이번 문제는 코드를 작성하면 할수록 생각보다 길어져서 ‘이렇게 푸는 게 맞나’라는 생각이 든 문제였다.
+ * 먼저 문제의 답이 담겨있는 answers 배열의 길이만큼 for문을 돌려 수포자들이 찍은 문제들과 맞춰보고
+ * 답이 맞으면 cnt 배열에 값을 증가시킨다. 나는 cnt 배열의 값을 유지하기 위해 temp 배열에 cnt의 값을 옮겨 담고
+ * temp을 정렬시켰다. 그리고 배열의 추가, 삭제를 쉽게 하기 위해 ArrayList인 result를 선언하고,
+ * or문을 또 돌려 정렬로 인해 제일 큰 값이 맨 마지막 인덱스에 있으니 temp[2]와 cnt[i]가 같으면 result에 i+1를 저장했다.
+ * 그리고 그걸 또 answer에 넣으려고 또 for문 사용..! 다른 사람들이 작성한 코드를 보고 내 코드를 다시 보니까 불필요한 부분이 많은 것 같다.
+ */
 public class solutionOne {
     int [] abc = {1,2,3,4,5};
     public int[] solution(int[] answers) {
