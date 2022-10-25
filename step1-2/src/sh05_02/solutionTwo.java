bd4eb7f6a22801426d340fb5d5085b3841ae0ed4
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-25 18:51:01 +0900

    20221025

---
+++ step1-2/src/sh05_02/solutionTwo.java
@@ -0,0 +1,19 @@
+package sh05_02;
+
+public class solutionTwo {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if (a > b) {
+            for (int i = b; i <= a; i++) {
+                answer = answer + i;
+            }
+        } else if (b > a) {
+            for (int i = a; i <= b; i++) {
+                answer = answer + i;
+            }
+        } else {
+            answer = a;
+        }
+        return answer;
+    }
+}
