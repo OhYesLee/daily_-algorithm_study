189e98851e81d18ae785b51aa644712fcb598c1e
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-25 23:18:06 +0900

    20221025

---
+++ step1-2/src/ch05_03/solutionOne.java
@@ -0,0 +1,16 @@
+package ch05_03;
+
+public class solutionOne {
+    public String solutionOne(int n) {
+        String answer = "";
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) answer += "수";
+            else answer += "박";
+        }
+        return answer;
+    }
+
+//    public int showSolutionOne(){
+//        return Integer.parseInt(solution(3));
+//    }
+}
