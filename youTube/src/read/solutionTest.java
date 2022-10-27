4d4775fa95130b8f5d565d3d1db8485f0b0bb27b
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-27 09:26:24 +0900

    20221027

---
+++ youTube/src/read/solutionTest.java
@@ -0,0 +1,11 @@
+package read;
+
+public class solutionTest {
+    public static void main(String[] args) {
+        String[] part = {"leo", "kiki", "eden"};
+        String[] comp = {"eden", "kiki"};
+        solution solution = new solution();
+//        Solution sol = new Solution();
+        System.out.println(solution.solution(part,comp));
+    }
+}
