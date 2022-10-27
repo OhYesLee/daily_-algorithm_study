fed259e47d62b99b872b92636960a01cd73ddb71
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-27 09:44:28 +0900

    20221027

---
+++ youTube/src/read2/solutionTest.java
@@ -0,0 +1,9 @@
+package read2;
+
+public class solutionTest {
+    public static void main(String[] args) {
+        solution sol = new solution();
+        int[] priorities = { 2, 1, 3, 2 };
+        System.out.print(sol.solution(priorities, 2));
+    }
+}
