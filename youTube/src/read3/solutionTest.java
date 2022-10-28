6015db9a30e4412e5df64e64a505238f0f1cd244
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-28 10:19:30 +0900

    20221028

--- youTube/src/read3/solutionTest.java
+++ youTube/src/read3/solutionTest.java
@@ -6,5 +6,12 @@ public class solutionTest {
         int[] lost = {2, 4};
         int[] reserve = {1, 3, 5};
         System.out.print(sol.solution(5, lost, reserve));
+        System.out.println();
+        System.out.println("==================");
+        SolutionTwo sol2 = new SolutionTwo();
+        int[] lost2 = {2, 4};
+        int[] reserve2 = {1, 3, 5};
+        System.out.print(sol2.solution(5, lost2, reserve2));
+        System.out.println();
     }
 }
