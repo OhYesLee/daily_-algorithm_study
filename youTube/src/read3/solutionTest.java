6defb6e77292de9aa5a8274982b9fc3a669e4280
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-28 10:11:24 +0900

    20221028

---
+++ youTube/src/read3/solutionTest.java
@@ -0,0 +1,10 @@
+package read3;
+
+public class solutionTest {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+        System.out.print(sol.solution(5, lost, reserve));
+    }
+}
