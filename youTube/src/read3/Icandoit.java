1ddf25fa64375a81ec244e76e75d4ec1f2d8d922
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-28 10:32:29 +0900

    20221028

--- youTube/src/read3/Icandoit.java
+++ youTube/src/read3/Icandoit.java
@@ -3,6 +3,8 @@ package read3;
 public class Icandoit {
     public static void main(String[] args) {
         System.out.println("할 수 있다. ");
-        
+        System.out.println("매일 매일 한걸음씩");
+        System.out.println("좌절하지 않는다? 좌절해도 괜찮다");
+        System.out.println("매일 조금씩 나아가자.");
     }
 }
