1edcb40b94b51347a14cef1dc79abeef874d866d
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-29 18:03:20 +0900

    20221029

---
+++ youTube/src/read4/dailyRecord.java
@@ -0,0 +1,9 @@
+package read4;
+
+public class dailyRecord {
+    public static void main(String[] args) {
+        System.out.println("소리내서 책읽기");
+        System.out.println("유튜브로 기록하기");
+        System.out.println("매일 코딩하기");
+    }
+}
