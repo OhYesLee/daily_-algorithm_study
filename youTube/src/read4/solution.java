ab1a5fa379ca940b4258c48792f1061206406a3c
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-29 18:00:01 +0900

    20221029

--- youTube/src/read4/solution.java
+++ youTube/src/read4/solution.java
@@ -1,5 +1,24 @@
 package read4;
 
+import java.util.Scanner;
+
 public class solution {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        sc.close();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                System.out.print(String.valueOf(c).toLowerCase());
+            } else {
+                System.out.print(String.valueOf(c).toUpperCase());
+                System.out.println();
+            }
+        }
+    }
 }
