d447d5e6081a95fc3afc70108896b8f7b768ae77
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-31 23:00:15 +0900

    20221031

--- dailyRecord/src/test1031/today.java
+++ dailyRecord/src/test1031/today.java
@@ -4,6 +4,7 @@ public class today {
     public static void main(String[] args) {
         System.out.println("오늘도 했다.");
         System.out.println("오늘도 성공했다.");
-        System.out.println();
+        System.out.println("내일도 성공한다.");
+        System.out.println("힘을 내자 이성욱!");
     }
 }
