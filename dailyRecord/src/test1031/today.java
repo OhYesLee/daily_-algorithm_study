472035a07040f0b0ad9939dc5737f64618a3aade
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-31 22:59:35 +0900

    20221031

---
+++ dailyRecord/src/test1031/today.java
@@ -0,0 +1,9 @@
+package test1031;
+
+public class today {
+    public static void main(String[] args) {
+        System.out.println("오늘도 했다.");
+        System.out.println("오늘도 성공했다.");
+        System.out.println();
+    }
+}
