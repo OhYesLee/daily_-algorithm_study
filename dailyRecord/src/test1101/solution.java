54d12a839e4de620c5a587c0434fe556a5418c5f
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-01 15:28:44 +0900

    20221101

--- dailyRecord/src/test1101/solution.java
+++ dailyRecord/src/test1101/solution.java
@@ -1,5 +1,19 @@
 package test1101;
 
+import java.util.Arrays;
+
 public class solution {
+    public String sol(String s) {
+        String[] numStr = s.split(" ");
+        int[] nums = new int[numStr.length];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(numStr[i]);
+        }
+
+        Arrays.sort(nums);
+
+        return nums[0] + " " + nums[nums.length - 1];
+    }
+
 
 }
--- dailyRecord/src/test1101/solutionTest.java
+++ dailyRecord/src/test1101/solutionTest.java
@@ -2,6 +2,9 @@ package test1101;
 
 public class solutionTest {
     public static void main(String[] args) {
-        
+
+        int[] arr = {1,2,3,4};
+        solution solution = new solution();
+        System.out.println();
     }
 }
