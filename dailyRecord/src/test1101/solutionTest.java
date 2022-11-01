4e4bff9f7b914cd686b4b1a28104f639313100f5
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-01 15:35:01 +0900

    20221101

--- dailyRecord/src/test1101/solutionTest.java
+++ dailyRecord/src/test1101/solutionTest.java
@@ -5,8 +5,17 @@ import java.util.Arrays;
 public class solutionTest {
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3, 4};
+//        int[] arr = {1, 2, 3, 4};
+        String str1 = "1 2 3 4";
         solution solution = new solution();
-//        System.out.println(Arrays.toString(solution.sol(arr)));
+        System.out.println("최대값과 최소값은 ====> "+solution.sol(str1));
+
+        System.out.println("==========");
+
+        String str = "1 2 3 4";
+        solutionTwo minMax = new solutionTwo();
+        //아래는 테스트로 출력해 보기 위한 코드입니다.
+        System.out.println("최대값과 최소값은?" + minMax.getMinMaxString(str));
+
     }
 }
