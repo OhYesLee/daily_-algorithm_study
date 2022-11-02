f276e2e1a3121ba892bab1a58c4168c58e2eb51e
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-02 23:13:08 +0900

    20221102

--- dailyRecord/src/test1102/solutionTest.java
+++ dailyRecord/src/test1102/solutionTest.java
@@ -2,6 +2,7 @@ package test1102;
 
 public class solutionTest {
     public static void main(String[] args) {
-        
+        solution solution = new solution();
+        System.out.println(solution.solution(8, 12));
     }
 }
