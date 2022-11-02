0f2540e4d214a7c70b52468776b2396a94e447d0
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-02 23:11:57 +0900

    20221102

--- dailyRecord/src/test1102/solution.java
+++ dailyRecord/src/test1102/solution.java
@@ -2,6 +2,13 @@ package test1102;
 
 import java.math.BigInteger;
 
+/**
+ * math 라이브러리의 BigInteger을 사용하여 최대공약수(GCD)를 간단하게 구하였다.
+ * 그 뒤에 문제에 나온 조건에 대한 식을 사용하여 문제를 해결했다.
+ * w길이와 h길이의 최대 공약수 만큼 사용할 수 없는 사각형의 덩어리가 나오고
+ * 해당 사각형의 갯수를 구하는 방법은 w길이를 최대공약수로 나눈 수와 h길이를
+ * 최대공약수로 나눈 수를 더한 뒤 1을 빼주면 나오게 된다.
+ */
 public class solution {
     public long solution(int w, int h) {
         int gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).intValue();
---
+++ dailyRecord/src/test1102/solutionTest.java
@@ -0,0 +1,7 @@
+package test1102;
+
+public class solutionTest {
+    public static void main(String[] args) {
+        
+    }
+}
