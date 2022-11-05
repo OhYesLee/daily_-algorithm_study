0b65ad02b9528841d428f189d0d2d42c4070b8d5
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-05 23:47:43 +0900

    20221105

--- dailyRecord/src/test1105/solutionTest.java
+++ dailyRecord/src/test1105/solutionTest.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class solutionTest {
     public static void main(String[] args) {
-        int [] arr = ("1,2,3,4") ;
+        int[] arr = {1, 2, 3, 4};
         solutionTwo solutionTwo = new solutionTwo();
-        System.out.println(Arrays.toString(solutionTwo.solutionTwo()));
+//        System.out.println(Arrays.toString(solutionTwo.solutionTwo("1,2,3,4")));
+        System.out.println(solutionTwo.solutionTwo("1,2,3,4"));
 
     }
 }
