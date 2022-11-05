a62429417eb83799266fef9450cf4f1f90c0c77b
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-05 23:43:20 +0900

    20221105

--- dailyRecord/src/test1105/solutionTest.java
+++ dailyRecord/src/test1105/solutionTest.java
@@ -2,6 +2,8 @@ package test1105;
 
 public class solutionTest {
     public static void main(String[] args) {
+        solutionTwo solutionTwo = new solutionTwo();
+        System.out.println(solutionTwo.solutionTwo());
 
     }
 }
--- dailyRecord/src/test1105/solutionTwo.java
+++ dailyRecord/src/test1105/solutionTwo.java
@@ -1,5 +1,17 @@
 package test1105;
 
+import java.util.Arrays;
+
 public class solutionTwo {
-    
+    public String solutionTwo(String s) {
+        String[] numStr = s.split(" ");
+        int[] nums = new int[numStr.length];
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = Integer.parseInt(numStr[i]);
+        }
+
+        Arrays.sort(nums);
+
+        return nums[0] + " " + nums[nums.length-1];
+    }
 }
