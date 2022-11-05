a9598728d558cae15535fd4aabca0d8cc5815968
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-05 23:45:57 +0900

    20221105

--- dailyRecord/src/test1105/solutionTest.java
+++ dailyRecord/src/test1105/solutionTest.java
@@ -1,9 +1,12 @@
 package test1105;
 
+import java.util.Arrays;
+
 public class solutionTest {
     public static void main(String[] args) {
+        int [] arr = ("1,2,3,4") ;
         solutionTwo solutionTwo = new solutionTwo();
-        System.out.println(solutionTwo.solutionTwo());
+        System.out.println(Arrays.toString(solutionTwo.solutionTwo()));
 
     }
 }
