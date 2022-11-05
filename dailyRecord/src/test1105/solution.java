3dde678e1108968517a7ada9e75914c021e4af31
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-05 23:38:41 +0900

    20221105

--- dailyRecord/src/test1105/solution.java
+++ dailyRecord/src/test1105/solution.java
@@ -1,4 +1,37 @@
 package test1105;
 
+import java.util.Scanner;
+
 public class solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int s, e;
+        while (true) {
+            s = sc.nextInt();
+            e = sc.nextInt();
+            if ((s >= 2 && s <= 9) && (e >= 2 && e <= 9)) break;
+            else {
+                System.out.println("INPUT ERROR!");
+            }
+        }
+
+        if (s < e) {
+            for (int i = 1; i <= 9; i++) {
+                for (int j = s; j <= e; j++) {
+                    System.out.printf("%d * %d = %2d", j, i, j * i);
+                    if (j != e) System.out.print("   ");
+                }
+                System.out.println();
+            }
+        } else {
+            for (int i = 1; i <= 9; i++) {
+                for (int j = s; j >= e; j--) {
+                    System.out.printf("%d * %d = %2d", j, i, j * i);
+                    if (j != e) System.out.print("   ");
+                }
+                System.out.println();
+            }
+        }
+    }
 }
