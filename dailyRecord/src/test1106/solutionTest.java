f805dd93f156cd004bbb4ce48dc7ed350c89f48e
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-06 17:35:05 +0900

    20221106

--- dailyRecord/src/test1106/solutionTest.java
+++ dailyRecord/src/test1106/solutionTest.java
@@ -2,6 +2,10 @@ package test1106;
 
 public class solutionTest {
     public static void main(String[] args) {
+        solution solution = new solution();
+        System.out.println(solution.solution(5));
+        System.out.println(solution.solution(3));
+
 
     }
 }
