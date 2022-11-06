fbee443db21bbb3a5d656fd1d5d928ecb60dc760
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-06 17:40:25 +0900

    20221106

--- dailyRecord/src/dailyRecord.java
+++ dailyRecord/src/dailyRecord.java
@@ -3,5 +3,6 @@ public class dailyRecord {
         System.out.println("소리내서 책읽기");
         System.out.println("유튜브로 기록하기");
         System.out.println("매일 코딩하기");
+        System.out.println("========");
     }
 }
