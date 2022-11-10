f98342d7b9ac9bfd203fe67cba53ec6222816590
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-10 23:02:32 +0900

    20221110

---
+++ dailyRecord/src/test1110/solutionTest.java
@@ -0,0 +1,12 @@
+package test1110;
+
+import java.util.Arrays;
+
+public class solutionTest {
+    public static void main(String[] args) {
+        int [] arr = {1,3,2,5,4};
+        int arr1 = 9;
+        solution solution = new solution();
+        System.out.println(Arrays.toString(new int[]{solution.solution(arr, arr1)}));
+    }
+}
