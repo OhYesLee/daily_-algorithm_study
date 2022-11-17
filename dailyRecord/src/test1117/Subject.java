e2a2e8905b0f8fb5abf436df10c1dc2d95da8831
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-17 22:39:22 +0900

    20221117

---
+++ dailyRecord/src/test1117/Subject.java
@@ -0,0 +1,23 @@
+package test1117;
+
+public class Subject {
+    private String name;
+    private int scorePoint;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScorePoint() {
+        return scorePoint;
+    }
+
+    public void setScorePoint(int scorePoint) {
+        this.scorePoint = scorePoint;
+    }
+
+}
