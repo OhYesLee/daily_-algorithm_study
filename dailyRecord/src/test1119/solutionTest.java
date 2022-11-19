469348144e5cf4eb0fe37ff328d0a0b313c51a57
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-19 23:47:43 +0900

    20221119

---
+++ dailyRecord/src/test1119/solutionTest.java
@@ -0,0 +1,17 @@
+package test1119;
+
+public class solutionTest {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+        System.out.print(sol.solution(5, lost, reserve));
+        System.out.println();
+        System.out.println("==================");
+        SolutionTwo sol2 = new SolutionTwo();
+        int[] lost2 = {2, 4};
+        int[] reserve2 = {1, 3, 5};
+        System.out.print(sol2.solution(5, lost2, reserve2));
+        System.out.println();
+    }
+}
