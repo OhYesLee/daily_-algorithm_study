1336032b9fbcda0abe3d08b5c50b18d2d27c9ad3
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-20 22:41:37 +0900

    20221120

---
+++ dailyRecord/src/test1120/solutionTest.java
@@ -0,0 +1,14 @@
+package test1120;
+
+public class solutionTest {
+    public static void main(String[] args) {
+        solution sol = new solution();
+        int[] priorities = { 2, 1, 3, 2 };
+        System.out.print(sol.solution(priorities, 2));
+
+        System.out.println("==========");
+        solutionTwo solutionTwo = new solutionTwo();
+        int[] priorities1 = { 2, 1, 3, 2 };
+        System.out.print(solutionTwo.solution(priorities1,2));
+    }
+}
