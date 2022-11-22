addcb2c544480b1e4a8f9747519f3786ccc22c89
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-22 21:16:41 +0900

    20221122

---
+++ dailyRecord/src/test1121/ComputerTest.java
@@ -0,0 +1,12 @@
+package test1121;
+
+public class ComputerTest {
+
+    public static void main(String[] args) {
+        Computer computer = new DeskTop();
+        computer.display();
+        computer.turnOff();
+
+        NoteBook myNote = new MyNoteBook();
+    }
+}
