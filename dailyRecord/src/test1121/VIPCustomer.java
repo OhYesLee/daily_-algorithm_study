c0ff47dbf8d66fea033714243bbc249c5c4a66da
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-23 21:29:16 +0900

    20221123

--- dailyRecord/src/test1121/VIPCustomer.java
+++ dailyRecord/src/test1121/VIPCustomer.java
@@ -2,14 +2,14 @@ package test1121;
 
 public class VIPCustomer {
     // super를 이용하여 상위 클래스의 생성자 명시적으로 호출
-    public VIPCustomer(int customerID, String customerName) {
-        super(customerID, customerName);
-
-        customerGrade = "VIP";
-        bonusRatio = 0.05;
-        salesRatio = 0.1;
-
-        System.out.println("VIPCustomer(int, String) 생성자 호출");
-    }
+//    public VIPCustomer(int customerID, String customerName) {
+//        super(customerID, customerName);
+//
+//        customerGrade = "VIP";
+//        bonusRatio = 0.05;
+//        salesRatio = 0.1;
+//
+//        System.out.println("VIPCustomer(int, String) 생성자 호출");
+//    }
 
 }
