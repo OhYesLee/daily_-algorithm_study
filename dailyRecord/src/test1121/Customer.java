cde344dfc94e133baf9c8ffaf758ab1bbd6b563c
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-23 21:27:35 +0900

    20221123

--- dailyRecord/src/test1121/Customer.java
+++ dailyRecord/src/test1121/Customer.java
@@ -3,12 +3,12 @@ package test1121;
 public class Customer {
     // 디폴트 생성자 없애고 매개 변수가 있는 생성자 추가
     public Customer(int customerID, String customerName) {
-        this.customerID = customerID;
-        this.customerName = customerName;
-
-        customerGrade = "SILVER";
-        bonusRatio = 0.01;
-        System.out.println("Customer(int, String) 생성자 호출");
+//        this.customerID = customerID;
+//        this.customerName = customerName;
+//
+//        customerGrade = "SILVER";
+//        bonusRatio = 0.01;
+//        System.out.println("Customer(int, String) 생성자 호출");
     }
 
 }
---
+++ dailyRecord/src/test20221123/CarTest.java
@@ -0,0 +1,12 @@
+package test20221123;
+
+public class CarTest {
+    public static void main(String[] args) {
+        Car aiCar = new AICar();
+        aiCar.run();
+        System.out.println("=================");
+        Car manualCar = new ManualCar();
+        manualCar.run();
+    }
+
+}
