34c25a08906e5273fb5859da4e1264928148f86c
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-25 20:31:39 +0900

    20221125

---
+++ dailyRecord/src/test1125/UserInfoOracleDao.java
@@ -0,0 +1,17 @@
+package test1125;
+
+public class UserInfoOracleDao implements UserInfoDao {
+
+    public void insertUserInfo(UserInfo userInfo) {
+        System.out.println("insert into ORACLE DB userId =" + userInfo.getUserId());
+    }
+
+    public void updateUserInfo(UserInfo userInfo) {
+        System.out.println("update into ORACLE DB userId = " + userInfo.getUserId());
+    }
+
+    public void deleteUserInf(UserInfo userInfo) {
+        System.out.println("delete from ORACLE DB userId = " + userInfo.getUserId());
+    }
+}
+
