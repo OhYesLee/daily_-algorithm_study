c9878f37985181235ed5218b25c1505010b84379
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-25 20:30:17 +0900

    20221125

---
+++ dailyRecord/src/test1125/UserInfoDao.java
@@ -0,0 +1,10 @@
+package test1125;
+
+public interface UserInfoDao {
+    void insertUserInfo(UserInfo userInfo);
+
+    void updateUserInfo(UserInfo userInfo);
+
+    void deleteUserInf(UserInfo userInfo);
+
+}
