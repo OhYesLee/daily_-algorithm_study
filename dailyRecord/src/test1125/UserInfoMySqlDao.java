c5f4c9d5247e86018ae8b2603d36c83808985f91
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-25 20:30:46 +0900

    20221125

---
+++ dailyRecord/src/test1125/UserInfoMySqlDao.java
@@ -0,0 +1,22 @@
+package test1125;
+
+public class UserInfoMySqlDao implements UserInfoDao {
+
+    @Override
+    public void insertUserInfo(UserInfo userInfo) {
+        System.out.println("insert into MYSQL DB userId =" + userInfo.getUserId());
+    }
+
+    @Override
+    public void updateUserInfo(UserInfo userInfo) {
+        System.out.println("update into MYSQL DB userId = " + userInfo.getUserId());
+    }
+
+    @Override
+    public void deleteUserInf(UserInfo userInfo) {
+        System.out.println("delete from MYSQL DB userId = " + userInfo.getUserId());
+
+    }
+
+}
+
