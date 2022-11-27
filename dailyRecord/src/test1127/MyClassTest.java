33668faa1a711aa499fc07a296ef73d339ac1d52
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-27 19:35:25 +0900

    20221127

---
+++ dailyRecord/src/test1127/MyClassTest.java
@@ -0,0 +1,23 @@
+package test1127;
+
+public class MyClassTest {
+
+    public static void main(String[] args) {
+
+        MyClass mClass = new MyClass();
+
+        X xClass = mClass;
+        xClass.x();
+
+
+        Y yClass = mClass;
+        yClass.y();
+
+        MyClass iClass = mClass;
+        iClass.x();
+        iClass.y();
+        iClass.myMethod();
+    }
+
+}
+
