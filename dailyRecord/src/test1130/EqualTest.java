69811e205f377e81d620ddc04c46fd764451d3bb
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-30 23:56:19 +0900

    20221130

--- dailyRecord/src/test1130/EqualTest.java
+++ dailyRecord/src/test1130/EqualTest.java
@@ -16,6 +16,10 @@ public class EqualTest {
         Integer i1 = 100;
         Integer i2 = 100;
 
+//        Integer i1 = new Integer(100);
+//        Integer i2 = new Integer(100);
+
+
         System.out.println(i1.equals(i2));
         System.out.println(i1.hashCode());
         System.out.println(i2.hashCode());
