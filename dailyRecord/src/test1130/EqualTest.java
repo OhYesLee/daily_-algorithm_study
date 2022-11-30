d353fd11a882301b33a79c3032ab08dbea20be45
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-30 23:55:08 +0900

    20221130

---
+++ dailyRecord/src/test1130/EqualTest.java
@@ -0,0 +1,29 @@
+package test1130;
+
+public class EqualTest {
+    public static void main(String[] args) {
+        Student Lee = new Student(100, "Lee");
+        Student Lee2 = Lee;
+        Student Shun = new Student(100, "Lee");
+
+        System.out.println(Lee == Shun);
+        System.out.println(Lee.equals(Shun));
+
+        System.out.println(Lee.hashCode());
+        System.out.println(Shun.hashCode());
+
+
+        Integer i1 = 100;
+        Integer i2 = 100;
+
+        System.out.println(i1.equals(i2));
+        System.out.println(i1.hashCode());
+        System.out.println(i2.hashCode());
+
+        System.out.println(System.identityHashCode(i1));
+        System.out.println(System.identityHashCode(i2));
+
+    }
+}
+
+
